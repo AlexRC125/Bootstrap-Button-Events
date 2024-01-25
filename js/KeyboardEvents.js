c18750c015c5e5d4dac9ac360c1057316c054725
Alex <125aramoscortes@frhsd.com>
2024-01-25 09:48:47 -0500

    changes 3

---
+++ js/KeyboardEvents.js
@@ -0,0 +1,35 @@
+//Row 2
+//Keyboard Events
+
+//Key Down
+let inputOne= document.getElementById('inputOne');
+inputOne.addEventListener('keydown', function(e){
+    inputOne.value = e.code;
+    e.preventDefault()
+    console.log(e.code);
+});
+//key up
+let inputTwo= document.getElementById('inputTwo');
+inputTwo.addEventListener('keyup', function(e){
+    e.preventDefault();
+    inputTwo.value = e.code;
+});
+
+let inputThree= document.getElementById('inputThree');
+inputThree.addEventListener('keypress', function (){
+    var randomHex = Math.floor(maty.random()*16777215).toString(16);
+    inputThree.style.backgroundcolor = "#" + randomHex;
+});
+
+let inputFour= document.getElementById('inputFour');
+inputFour.addEventListener('keydown', function(e){
+    if(e.ctrlKey && e.shiftKey){
+        console.log('Both');
+        // Pass in the id of an element
+
+        (e.target.style.opacity = 0),
+        setTimeout(() =>{
+            e.target.style.visibility
+        })
+    }
+})
--- js/clickEvents.js
+++ js/clickEvents.js
@@ -1,3 +1,5 @@
+
+
 let btnOne = document.getElementById('click'); 
 btnOne.addEventListener('click', function(event){
     event.target.style.background = "#a0b96b";
